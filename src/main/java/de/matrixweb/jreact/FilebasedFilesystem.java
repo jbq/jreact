b72baf1f842e4604768cff9533f574fcb7f2cf2d
Jean-Baptiste Quenot <jbq@beorntech.com>
2015-11-30 16:58:59 +0100

    Add debug logging in FilebasedFilesystem

--- src/main/java/de/matrixweb/jreact/FilebasedFilesystem.java
+++ src/main/java/de/matrixweb/jreact/FilebasedFilesystem.java
@@ -4,7 +4,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class FilebasedFilesystem implements Filesystem {
+  private static final Logger _log = LogManager.getLogger(FilebasedFilesystem.class);
 
   private final String fileBase;
 
@@ -18,11 +22,13 @@ public class FilebasedFilesystem implements Filesystem {
 
   @Override
   public boolean isFile(final String fileName) {
+    _log.debug("Looking for " + fileName);
     return new File(this.fileBase, fileName.toString()).isFile();
   }
 
   @Override
   public String readFile(final String path) throws FileNotFoundException {
+    _log.debug("Read " + path);
     try (Scanner scanner = new java.util.Scanner(new java.io.File(this.fileBase, path), "UTF-8")) {
       return scanner.useDelimiter("\\Z").next().toString();
     }
@@ -41,6 +47,7 @@ public class FilebasedFilesystem implements Filesystem {
 
   @Override
   public boolean exists(final String fileName) {
+    _log.debug("Exists " + fileName);
     return new File(fileName).exists();
   }
 
